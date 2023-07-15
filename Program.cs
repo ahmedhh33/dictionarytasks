4ae0ac7e97820f190acfa6014b759e41d4ab8e21
ahmedhh33 <ahmedhh3633@gmail.com>
2023-07-15 22:56:44 +0400

    task2

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace DictionaryTask
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Task1 split");
             Console.WriteLine("Enter a sentence:");
             string sentence = Console.ReadLine();
 
@@ -16,6 +17,25 @@ namespace DictionaryTask
             {
                 Console.WriteLine($"{entry.Key}: {entry.Value}");
             }
+
+            Console.WriteLine("Task2 hashset uniq");
+            //Console.WriteLine("Enter 10 numbers:");
+            //int[] numbers = new int[3];
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    numbers[i] = int.Parse(Console.ReadLine());
+            //   // Console.WriteLine(numbers[i]);
+            //}
+            int[] numbers = { 1, 2, 3, 1, 56, 7, 2, 3 };
+
+            HashSet<int> Uniq = GetUniqnumber(numbers);
+            Console.Write("the uniqe words are: ");
+            foreach(int i in Uniq)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();
+
         }
         static Dictionary<string, int> CountWordFrequency(string sentence)
         {
@@ -30,6 +50,12 @@ namespace DictionaryTask
             }
 
             return wordFrequency;
+        }
+
+        public static HashSet<int> GetUniqnumber(int[] num)
+        {
+            HashSet<int> Uniqe = new HashSet<int>(num);
+            return Uniqe;
         }
 
 
