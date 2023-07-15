874afc3f544ac5e91ab7ac85e5900be90084d99f
ahmedhh33 <ahmedhh3633@gmail.com>
2023-07-15 22:09:53 +0400

    Dictionary task1

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DictionaryTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence:");
+            string sentence = Console.ReadLine();
+
+            Dictionary<string, int> wordFrequency = CountWordFrequency(sentence);
+
+            Console.WriteLine("Word Frequency:");
+            foreach (KeyValuePair<string, int> entry in wordFrequency)
+            {
+                Console.WriteLine($"{entry.Key}: {entry.Value}");
+            }
+        }
+        static Dictionary<string, int> CountWordFrequency(string sentence)
+        {
+            Dictionary<string, int> wordFrequency = new Dictionary<string, int>();
+
+            char[] breakchar = new char[] { ' ', '.', ',' };
+            string[] nwords = sentence.Split(breakchar, StringSplitOptions.RemoveEmptyEntries);
+
+            for(int i = 0;i<nwords.Length; i++)
+            {
+                wordFrequency.Add(nwords[i],i);
+            }
+
+            return wordFrequency;
+        }
+
+
+    }
+}
